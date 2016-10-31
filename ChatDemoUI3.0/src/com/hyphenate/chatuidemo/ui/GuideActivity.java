15f60d28890e2ed24e13f43ccf659cb330c86b78
abc908159305 <abc2052yang@outlook.com>
2016-10-31 20:32:23 +0800

    新增欢迎页面，使闪屏后跳转到此页面，并且在此页面增加登录、注册两个页面的跳转功能

---
+++ ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/GuideActivity.java
@@ -0,0 +1,25 @@
+package com.hyphenate.chatuidemo.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.hyphenate.chatuidemo.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+}
