30f32e7049d0bd69abe205ae84f7f37ae43cba6f
abc908159305 <abc2052yang@outlook.com>
2016-11-01 16:57:22 +0800

    导入NetDao包

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
@@ -0,0 +1,66 @@
+package cn.ucai.superwechat.data;
+
+import android.content.Context;
+
+import java.io.File;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.bean.Result;
+import cn.ucai.superwechat.utils.MD5;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class NetDao {
+
+    public static void login(Context context,String userName, String userPwd,
+                             OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(userPwd))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void register(Context context,String userName,String userNick,String userPwd,
+                             OkHttpUtils.OnCompleteListener<Result> listener) {
+        OkHttpUtils<Result> utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.NICK,userNick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(userPwd))
+                .targetClass(Result.class)
+                .post()
+                .execute(listener);
+    }
+    public static void updatenick(Context context,String userName, String userNick,
+                             OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.NICK,userNick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void updateAvatar(Context context, String username,
+                                    File file,OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+    public static void syncUserInfo(Context context, String username,
+                                    OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+}
