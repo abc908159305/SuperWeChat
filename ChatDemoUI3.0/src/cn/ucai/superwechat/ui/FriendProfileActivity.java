9a580a7a16a76cb3943bf37d4bb28a2a4b015c36
abc908159305 <abc2052yang@outlook.com>
2016-11-07 18:49:01 +0800

    新增详细资料Activty，并编写其布局

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -0,0 +1,21 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+
+import cn.ucai.superwechat.R;
+
+/**
+ * Created by Administrator on 2016/11/7.
+ */
+public class FriendProfileActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
