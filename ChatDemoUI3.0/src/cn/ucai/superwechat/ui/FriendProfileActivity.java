15eb5ee9e6d633cb396b5347eef4653f78431a32
abc908159305 <abc2052yang@outlook.com>
2016-11-07 19:09:09 +0800

    更改添加好友页面为微信风格，并实现显示搜索用户头像、昵称、微信号的功能

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -1,21 +1,61 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 /**
  * Created by Administrator on 2016/11/7.
  */
 public class FriendProfileActivity extends BaseActivity {
+    @Bind(R.id.iv_Back)
+    ImageView mivBack;
+    @Bind(R.id.tv_Title)
+    TextView mtvTitle;
+    @Bind(R.id.iv_Avatar)
+    ImageView mivAvatar;
+    @Bind(R.id.tv_userNick)
+    TextView mtvUserNick;
+    @Bind(R.id.tv_userName)
+    TextView mtvUserName;
+
+    User user = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_profile);
+        ButterKnife.bind(this);
+        user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
+        if (user == null) {
+            MFGT.finish(this);
+        }
         initView();
     }
 
     private void initView() {
-
+        mivBack.setVisibility(View.VISIBLE);
+        mtvTitle.setVisibility(View.VISIBLE);
+        mtvTitle.setText(getString(R.string.userinfo_txt_profile));
+        setUserInfo();
+    }
+    private void setUserInfo() {
+        EaseUserUtils.setAppUserAvatar(this,user.getMUserName(),mivAvatar);
+        EaseUserUtils.setAppUserNick(user.getMUserName(),mtvUserNick);
+        EaseUserUtils.setAppUserNameWithNo(user.getMUserName(),mtvUserName);
+    }
+    @OnClick(R.id.iv_Back)
+    public void onBack() {
+        MFGT.finish(this);
     }
 }
--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -134,6 +134,9 @@ public class EaseUserUtils {
         String username = EMClient.getInstance().getCurrentUser();
         setAppUserName("微信号：",username,textView);
     }
+    public static void setAppUserNameWithNo(String username,TextView textView) {
+        setAppUserName("微信号：",username,textView);
+    }
     public static void setCurrentAppUserName(TextView textView) {
         String username = EMClient.getInstance().getCurrentUser();
         setAppUserName("",username,textView);
